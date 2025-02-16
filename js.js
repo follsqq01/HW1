e99c24806ebfea5e86ffab9967de997c53e86567
follsqq01 <ruslanmelkumian80@yandex.ru>
2025-02-16 22:30:44 +0300

    main
    ready

---
+++ js.js
@@ -0,0 +1,18 @@
+// 1 задача
+let str = 'abcde';
+alert(str[0]);
+alert(str[1]);
+alert(str[4]);
+
+// 2 задача
+let a = parseInt(prompt("Введите первое число"));
+let b = parseInt(prompt("Введите второе число"));
+console.log(a > 0 || b > 0);
+
+// 3 задача
+let age = parseInt(prompt("Введите возраст"));
+if (age >= 18) {
+    alert("Совершеннолетний"); 
+} else {
+        alert("Несовершеннолетний"); 
+}
